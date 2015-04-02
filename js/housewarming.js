001e51f649aae04dd9715ee242a2dddcad5ac6cc
dvberkel <daan.v.berkel.1980@gmail.com>
2015-04-02 14:56:12 +0200

    Create a background view

--- js/housewarming.js
+++ js/housewarming.js
@@ -16,19 +16,31 @@
 	this.context.restore();
     }
 
+    var BackgroundView = function(canvas, context){
+	this.canvas = canvas;
+	this.context = context;
+	this.update();
+    };
+    BackgroundView.prototype.update = function(){
+	this.context.save();
+	this.context.fillStyle = 'rgba(0,0,0,0.1)';
+	this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+	this.context.restore();
+    }
+
+
     var View = $.View = function(house, canvas){
 	this.house = house;
 	this.canvas = canvas;
 	this.context = this.canvas.getContext('2d');
 	this.scenes = [
+	    new BackgroundView(this.canvas, this.context),
 	    new HouseView(house, this.context)
 	]
 	this.update();
     };
     View.prototype.update = function(){
-	this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-	this.context.fillStyle = 'rgba(0,0,0,0.1)';
-	this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
 	this.scenes.forEach(function(scene){
 	    scene.update();
 	});
